5946d94a17ad9f37d3ba9eee847c4d6adc0f4dc5
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-02-27 22:23:58 +0545

    update script

--- script.js
+++ script.js
@@ -7,19 +7,31 @@ const count = 10;
 const apiKey = "aN5Wd7rPU1wO8T1q-pX71CQQMbsnZ0EabQNykTsweJM";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+//Helper Function to set attributes on DOM elemetns
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
 //Create Elements for links and photos, and add to DOM
 function displayPhotos() {
   //Run function for each object in photosArray
   photosArray.forEach((photo) => {
     //Create a to link to unsplash
-    const item = document.createElement("a");
-    item.setAttribute("href", photo.link.html);
-    item.setAttribute("target", "_blank");
+    const item = document.createElement('a');
+
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
     //Create img for phots
-    const img = document.createElement("img");
-    img.setAttribute("src", photo.urls.regular);
-    img.setAttribute("alt", photo.alt_description);
-    img.setAttribute("title", photo.alt_description);
+    const img = document.createElement('img');
+
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
     //put img inside a then put both inside image container elements
     item.appendChild(img);
     imageContainer.appendChild(item);
