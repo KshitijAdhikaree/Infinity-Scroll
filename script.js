dfa13370a6890bcb97f2f7243f4f247c1b89764d
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-02-28 20:53:34 +0545

    count update to 5

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 30;
+const count = 5;
 const apiKey = "aN5Wd7rPU1wO8T1q-pX71CQQMbsnZ0EabQNykTsweJM";
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
